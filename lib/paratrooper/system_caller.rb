2af9dcf6e3428bb0a1fd212b6604568fd1314a84
Stuart McKears <stuart@ipsoot.com>
2013-02-14 17:00:27 +0000

    Add some debug around execute

--- lib/paratrooper/system_caller.rb
+++ lib/paratrooper/system_caller.rb
@@ -1,7 +1,11 @@
 module Paratrooper
   class SystemCaller
     def execute(call)
+      puts "Executing #{call}"
+      puts
       system(call)
+      puts
+      puts "Completed #{call}"
     end
   end
 end
